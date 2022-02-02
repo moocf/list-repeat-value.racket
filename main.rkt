8647ec58ce7483a79fe3c49cc2ce4a078e95f5f2
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:03:23 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-; (duple n x): N X N -> List(N)
+; (list.repeat-value n x): N X N -> List(N)
 ; returns a list containing n copies of x
 ; : n=0  -> ()
-; : else -> (x . (duple n-1 x))
-(define duple
+; : else -> (x . (list.repeat-value n-1 x))
+(define list.repeat-value
   (lambda (n x)
     (if (<= n 0)
         (list)
-        (cons x (duple (- n 1) x)))))
+        (cons x (list.repeat-value (- n 1) x)))))
